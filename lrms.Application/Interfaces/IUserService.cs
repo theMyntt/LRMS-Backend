ded069f77c56459b64ea32ad56b74ff765c2a5af
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 14:29:59 -0300

    chore: user service contract

---
+++ lrms.Application/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+using System;
+using lrms.Application.DTOs;
+
+namespace lrms.Application.Interfaces;
+
+public interface IUserService
+{
+    Task<LoginOutputDTO> Login(LoginInputDTO dto);
+    Task<UserInsertDTO> Insert(UserInsertDTO dto);
+}
