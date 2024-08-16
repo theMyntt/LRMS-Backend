8bbb569feea25f8eddf27082771bc64556e2790e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 15:04:35 -0300

    chore: unique email migration

--- lrms.Infra.Data/Migrations/20240816180307_UniqueEmail.Designer.cs
+++ lrms.Infra.Data/Migrations/20240816180307_UniqueEmail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using lrms.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using lrms.Infra.Data.Context;
 namespace lrms.Infra.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240816180307_UniqueEmail")]
+    partial class UniqueEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
