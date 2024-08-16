24bf7ec7712f2ecde25ecf0070623843d5ec9f71
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 01:44:29 -0300

    chore: user entity core configuration

---
+++ lrms.Infra.Data/EntitiesConfiguration/UserEntityConfiguration.cs
@@ -0,0 +1,21 @@
+using System;
+using lrms.Infra.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace lrms.Infra.Data.EntitiesConfiguration;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(prop => prop.Id).IsRequired();
+        builder.Property(prop => prop.Name).IsRequired().HasMaxLength(128);
+        builder.Property(prop => prop.Email).IsRequired().HasMaxLength(128);
+        builder.Property(prop => prop.Password).IsRequired().HasMaxLength(255);
+        builder.Property(prop => prop.CreatedAt).IsRequired();
+
+        builder.HasOne<UserEntity>().WithMany().HasForeignKey(x => x.Id).OnDelete(DeleteBehavior.NoAction);
+    }
+}
