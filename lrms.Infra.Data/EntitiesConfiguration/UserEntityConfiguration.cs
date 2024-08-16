1af6fd470f431c2eb48b820d2111b8a6f78bb257
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 15:00:15 -0300

    fix: email is unique

--- lrms.Infra.Data/EntitiesConfiguration/UserEntityConfiguration.cs
+++ lrms.Infra.Data/EntitiesConfiguration/UserEntityConfiguration.cs
@@ -10,6 +10,9 @@ public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
     public void Configure(EntityTypeBuilder<UserEntity> builder)
     {
         builder.HasKey(x => x.Id);
+
+        builder.HasIndex(prop => prop.Email).IsUnique();
+
         builder.Property(prop => prop.Id).IsRequired();
         builder.Property(prop => prop.Name).IsRequired().HasMaxLength(128);
         builder.Property(prop => prop.Email).IsRequired().HasMaxLength(128);
