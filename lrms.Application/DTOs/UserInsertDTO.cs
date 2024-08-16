252b897a1abb53d750c4c3cc6c14f02bdb4244d3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 14:45:30 -0300

    fix: missing required annotatoin

--- lrms.Application/DTOs/UserInsertDTO.cs
+++ lrms.Application/DTOs/UserInsertDTO.cs
@@ -1,11 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace lrms.Application.DTOs;
 
 public class UserInsertDTO
 {
+    [Required]
     public required string Name { get; set; }
+    [Required]
     public required string Email { get; set; }
+    [Required]
     public required string Password { get; set; }
     public Guid? CreatedBy { get; set; }
 
