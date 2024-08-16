69305b34ec9909220cecab5f44b7176885005b14
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 14:36:54 -0300

    feat: user service entity

---
+++ lrms.Application/Services/UserService.cs
@@ -0,0 +1,47 @@
+using System;
+using lrms.Application.DTOs;
+using lrms.Application.Interfaces;
+using lrms.Domain.Aggregates;
+using lrms.Infra.Data.Interfaces;
+
+namespace lrms.Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _repository;
+
+    public UserService(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<UserInsertDTO?> Insert(UserInsertDTO dto)
+    {
+        UserAggregate aggregate = new(dto.Name,
+                                      dto.Email,
+                                      dto.Password,
+                                      dto.CreatedBy);
+
+        bool isOk = await _repository.Insert(aggregate);
+
+        if (isOk)
+        {
+            return dto;
+        }
+
+        return null;
+    }
+
+    public async Task<LoginOutputDTO?> Login(LoginInputDTO dto)
+    {
+        UserAggregate? aggregate = await _repository.Login(dto.Email, dto.Password);
+
+        if (aggregate == null) return null;
+
+        return new LoginOutputDTO
+        {
+            Id = aggregate.Id,
+            Name = aggregate.Name
+        };
+    }
+}
