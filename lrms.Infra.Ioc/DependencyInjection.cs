ceb5de30a07e01034e03d15104073dc3e4c430b8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-16 02:39:46 -0300

    chore: injecting user mapper and user repository

--- lrms.Infra.Ioc/DependencyInjection.cs
+++ lrms.Infra.Ioc/DependencyInjection.cs
@@ -1,5 +1,9 @@
 using System;
+using lrms.Domain.Aggregates;
 using lrms.Infra.Data.Context;
+using lrms.Infra.Data.Entities;
+using lrms.Infra.Data.Interfaces;
+using lrms.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +20,9 @@ public static class DependencyInjection
                              ServerVersion.AutoDetect(configuration.GetConnectionString("MySQL")),
                              options => options.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName));
         });
+
+        services.AddScoped<IMapper<UserEntity, UserAggregate>, UserMapper>();
+        services.AddScoped<IUserRepository, UserRepository>();
 
         return services;
     }
